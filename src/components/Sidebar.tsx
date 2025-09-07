95bcdaf5404c6d55d0b32d3c3f22b94751a06837
karifol <karigolhorinouchi@gmail.com>
2025-09-07 15:05:12 +0900

    u

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className="space-y-4 p-5 h-screen flex flex-col bg-gray-100">
       {/* チャットを新規作成 */}
       <button
-        className="space-y-1 flex items-center cursor-pointer hover:bg-gray-300 rounded p-2 font-bold border-0 bg-transparent"
+        className="space-y-1 flex items-center cursor-pointer hover:bg-gray-300 rounded p-2 border-0 bg-transparent text-sm"
         onClick={() => window.location.reload()}
         type="button"
       >
